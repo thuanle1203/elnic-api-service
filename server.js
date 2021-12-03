2088495848f9c019dc09144fc01a95476a733e27
ThuanLX <thuan.le@botstar.com>
2021-12-04 00:58:58 +0700

    fix-corx-again

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ require('dotenv').config();
 
 const morgan = require("morgan");
 
-const low = require("lowdb");
-
 const bodyParser = require("body-parser");
 
 const swaggerUI = require("swagger-ui-express");
@@ -18,10 +16,11 @@ const swaggerJsDoc = require("swagger-jsdoc");
 
 const db = require("./app/models");
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+	res.setHeader('Access-Control-Allow-Credentials', true);
 });
 
 app.use(bodyParser.json());
